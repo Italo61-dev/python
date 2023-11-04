e55188dd9bf69da21969bee2097dd3c7c0c39dd7
Italo61-dev <italosousa42541@gmail.com>
2023-11-03 21:20:31 -0300

    Create ex003.py

---
+++ exercicio-iniciantes/ex003.py
@@ -0,0 +1,6 @@
+#   Faça um progama que leia notas e depois mostre sua media
+
+oneNote = float(input('Digite sua primeira nota: '))
+twoNote = float(input('Digite sua segunda nota: '))
+avarage = (oneNote + twoNote) / 2
+print('Sua média final é de {:.2f}'.format(avarage))
