2c50d25e6a933b654461976d1988eeabd251081b
Italo61-dev <italosousa42541@gmail.com>
2023-11-03 21:22:57 -0300

    Create ex005.py

---
+++ exercicio-iniciantes/ex005.py
@@ -0,0 +1,15 @@
+#   Faça um progama de tabuada
+
+tab = int(input('Digite um número para saber sua tabuada: '))
+m0 = tab * 0 
+m1 = tab * 1
+m2 = tab * 2
+m3 = tab * 3
+m4 = tab * 4
+m5 = tab * 5
+m6 = tab * 6
+m7 = tab * 7
+m8 = tab * 8
+m9 = tab * 9
+m10 = tab * 10
+print(f'Seu número é {tab}\n{tab} x 0 = {m0}\n{tab} x 1 = {m1}\n{tab} x 2 = {m2}\n{tab} x 3 = {m3}\n{tab} x 4 = {m4}\n{tab} x 5 = {m5}\n{tab} x 6 = {m6}\n{tab} x 7 = {m7}\n{tab} x 8 = {m8}\n{tab} x 9 = {m9}\n{tab} x 10 = {m10}')
