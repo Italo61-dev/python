ac03c1b59a024d441eb0bc4c71cc31c89b139ff9
Italo61-dev <italosousa42541@gmail.com>
2023-11-03 21:17:23 -0300

    Create ex002.py

---
+++ exercicio-iniciantes/ex002.py
@@ -0,0 +1,7 @@
+#   Faça um progama que leia seu drobro seu triplo e sua raiz quadrada
+
+value = float(input('Digite um valor: '))
+double = value * 2
+triple = value * 3
+squareRoot = value ** 0.5
+print('Seu valor é {}, seu dobro é {}, seu triplo {} e sua raiz quadrada é {:.2f}'.format(value, triple, squareRoot))   
