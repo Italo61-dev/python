d6d224ab80d1b5f7678e273aa11cd09fb55ab230
Italo61-dev <italosousa42541@gmail.com>
2024-03-19 20:37:49 -0300

    Update app.py

--- SABOR EXPRESS/app.py
+++ SABOR EXPRESS/app.py
@@ -1,5 +1,6 @@
 import os
-print("""
+def exibir_nome_progama():
+    print("""
     
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗░█████╗░
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝██╔══██╗
@@ -7,22 +8,34 @@ print("""
 ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗██║░░██║
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝╚█████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░░╚════╝░
-      """)
+""")
 
-print('1. Cadastrar restaurante\n2. Listar restaurantes\n3. Ativar restaurante\n4. Sair')
-opcao_escolha = int(input('Escolha uma opção: '))
+def exibir_opcoes():
+    print('1. Cadastrar restaurante\n2. Listar restaurantes\n3. Ativar restaurante\n4. Sair')
 
 def finalizar_app():
     os.system('cls')
     # os.system('clear')
     print('Finalizando o app\n')
 
+def escolher_opcao():
+    opcao_escolha = int(input('Escolha uma opção: '))
 
-if opcao_escolha == 1:
-    print('Cadastrar restaurante')
-elif opcao_escolha == 2: 
-    print('listar restaurantes')
-elif opcao_escolha == 3: 
-    print('Ativar restaurante')
-else:
-    finalizar_app()
+
+
+    if opcao_escolha == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolha == 2: 
+        print('listar restaurantes')
+    elif opcao_escolha == 3: 
+        print('Ativar restaurante')
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_progama()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
