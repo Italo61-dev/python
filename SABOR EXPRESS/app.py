fdf64c6843eb7cd0b02219a3b53767eac8d263ed
Italo61-dev <italosousa42541@gmail.com>
2024-03-19 20:13:46 -0300

    Create app.py

---
+++ SABOR EXPRESS/app.py
@@ -0,0 +1,10 @@
+opcao_escolha = int(input('Escolha uma opção: '))
+
+if opcao_escolha == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolha == 2: 
+    print('listar restaurantes')
+elif opcao_escolha == 3: 
+    print('Ativar restaurante')
+else:
+    print('Encerrando o progam')
