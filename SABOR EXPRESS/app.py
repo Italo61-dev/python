f07763de9c67005b3427a85928ce77fe0b26a45c
Italo61-dev <italosousa42541@gmail.com>
2024-03-21 19:46:45 -0300

    Update app.py

--- SABOR EXPRESS/app.py
+++ SABOR EXPRESS/app.py
@@ -18,21 +18,30 @@ def finalizar_app():
     # os.system('clear')
     print('Finalizando o app\n')
 
+def opcao_invalida():
+    print('Opção inválida!\n')
+    input('Digite qualquer tecla para voltar ao menu principal.')
+    main()
+    
+
 def escolher_opcao():
     opcao_escolha = int(input('Escolha uma opção: '))
-
-
-
-    if opcao_escolha == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolha == 2: 
-        print('listar restaurantes')
-    elif opcao_escolha == 3: 
-        print('Ativar restaurante')
-    else:
-        finalizar_app()
+    try:
+        if opcao_escolha == 1:
+            print('Cadastrar restaurante')
+        elif opcao_escolha == 2: 
+            print('listar restaurantes')
+        elif opcao_escolha == 3: 
+            print('Ativar restaurante')
+        elif opcao_escolha == 4: 
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('cls')
     exibir_nome_progama()
     exibir_opcoes()
     escolher_opcao()
